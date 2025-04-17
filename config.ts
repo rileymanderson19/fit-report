e4f83859bf73c40cb2dd6c0b78c3404a283da35e
rileymanderson19 <riley@rileymanderson.com>
2025-04-17 16:03:59 -0400

    config changes

--- config.ts
+++ config.ts
@@ -23,16 +23,16 @@ const config = {
         // REQUIRED — we use this to find the plan in the webhook (for instance if you want to update the user's credits based on the plan)
         priceId:
           process.env.NODE_ENV === "development"
-            ? "price_1Niyy5AxyNprDp7iZIqEyD2h"
+            ? "price_1REyI72ceCzkqxuBEJy3ZPfS"
             : "price_456",
         //  REQUIRED - Name of the plan, displayed on the pricing page
         name: "Starter",
         // A friendly description of the plan, displayed on the pricing page. Tip: explain why this plan and not others
         description: "Perfect for small projects",
         // The price you want to display, the one user will be charged on Stripe.
-        price: 99,
+        price: 79,
         // If you have an anchor price (i.e. $29) that you want to display crossed out, put it here. Otherwise, leave it empty
-        priceAnchor: 149,
+       // priceAnchor: 149,
         features: [
           {
             name: "NextJS boilerplate",
